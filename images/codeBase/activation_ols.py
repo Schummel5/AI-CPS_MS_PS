9c54165c298d0a94f17f5e9ac2ede31f55fa57b0
Schummel5 <philip.schummel@uni-potsdam.de>
2025-02-06 00:52:10 +0100

    Changed activation_ols.py

--- images/codeBase/activation_ols.py
+++ images/codeBase/activation_ols.py
@@ -7,7 +7,7 @@ with open('/tmp/ai_system/knowledgeBase/currentOlsSolution.pkl','rb') as f: #/tm
   model = pickle.load(f)
 
 
-act_df = pd.read_csv(f'/tmp/ai_system/acticationBase/activation_data.csv')#/tmp/AIBAS_KURS_PS_MS/data/activation_data.csv
+act_df = pd.read_csv(f'/tmp/ai_system/activationBase/activation_data.csv')#/tmp/AIBAS_KURS_PS_MS/data/activation_data.csv
 
 x_train = act_df['Estimated EPS'] # Independent variable
 y_train = act_df['Actual EPS'] # Dependent variable
